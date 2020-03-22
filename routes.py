f6e9f4722f8914fa14e402bb458582ef94cbcc3c
DanyZurita <dzurita@cifpfbmoll.eu>
2020-03-22 21:12:31 +0100

    feat(routes): Añadir todas las rutas a los htmls

--- routes.py
+++ routes.py
@@ -4,3 +4,19 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/backend')
+def backend():
+    return render_template('Backend/index.html')
+
+@app.route('/insertar')
+def insertar():
+    return render_template('Backend/insertar.html')
+
+@app.route('/editar')
+def editar():
+    return render_template('Backend/editar.html')
+
+@app.route('/update')
+def update():
+    return index()
