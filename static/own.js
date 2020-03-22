2e0d0f9e0135ef8f4168b2776388934fe8c24732
DanyZurita <dzurita@cifpfbmoll.eu>
2020-03-22 20:16:27 +0100

    style(js): Tabular bien las indentaciones del script

--- static/own.js
+++ static/own.js
@@ -1,184 +1,190 @@
 
-        // GET
+// GET
 
-        function inventario() {
+function inventario() {
 
-            var miHeaders = new Headers();
+    var miHeaders = new Headers();
 
-            var miInit = { method: 'GET',
-                           headers: miHeaders,
-                           mode: 'cors',
-                           // cambiarlo a force-cache => carga del disco
-                           cache: 'default' };
+    var miInit = {
+        method: 'GET',
+        headers: miHeaders,
+        mode: 'cors',
+        // cambiarlo a force-cache => carga del disco
+        cache: 'default'
+    };
 
 
-            fetch('http://127.0.0.1:5000/inventario', miInit)
-                .then((response) => {
-                    if(response.ok) {
-                        console.log("Response Status:", response.status);
-                        console.log("Reponse statuts text:", response.statusText);
-                        response.json().then((json) => logItems(json))
-                    } else {
-                        console.log("Response Status:", response.status);
-                        console.log("Reponse statuts text:", response.statusText);  
-                    }  
-                })
-                .catch((error) => {
-                    console.log(error.message);
-                });
-        }
+    fetch('http://127.0.0.1:5000/inventario', miInit)
+        .then((response) => {
+            if (response.ok) {
+                console.log("Response Status:", response.status);
+                console.log("Reponse statuts text:", response.statusText);
+                response.json().then((json) => logItems(json))
+            } else {
+                console.log("Response Status:", response.status);
+                console.log("Reponse statuts text:", response.statusText);
+            }
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+}
 
-        // intentar cachear con la cabecera mirando network de chrome
+// intentar cachear con la cabecera mirando network de chrome
 
-        function logItems(items) {
-            const itemsList = document.querySelector('#itemList');
-            itemList.innerHTML = items.map((item, i) => {
-                return `
+function logItems(items) {
+    const itemsList = document.querySelector('#itemList');
+    itemList.innerHTML = items.map((item, i) => {
+        return `
                         <li>
                             <p id="item${i}"> ${item.name}  
                                             ${item.sell_in}  
                                             ${item.quality}</p>
                         </li>
                         `;
-            }).join('');
+    }).join('');
+}
+
+// POST
+// curl -d name="Conjured Mana Cake" -d sell_in=5 -d quality=8
+// http://192.168.0.19:5000/items
+
+//let data = {name: "Conjured Mana Cake",
+//          sell_in: 5,
+//          quality: 8};
+
+let formulario = document.querySelector('.add-item');
+formulario.addEventListener('submit', addItem);
+
+function addItem(e) {
+    e.preventDefault();
+    // elementos del formulario en un array-like object:
+    // form.elements 
+    // pag 396 libro rhino
+    logForm();
+
+    let data = {
+        name: this.elements.name.value,
+        // sellin => bad request, status 400
+        // sin valor => mensaje del servidor en consola
+        // en XHR and fetch: message: {sell_in: "sellIn required"}
+        sell_in: this.elements.sell_in.value,
+        quality: this.elements.quality.value
+    };
+
+    fetch('http://127.0.0.1:5000/items', {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
         }
+    })
+        .then((response) => {
+            if (response.ok) {
+                console.log("Response OK Status:", response.status);
+                console.log("Reponse OK status text:", response.statusText);
+            } else {
+                console.log("Response Status:", response.status);
+                console.log("Reponse statuts text:", response.statusText);
+            }
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+}
 
-        // POST
-        // curl -d name="Conjured Mana Cake" -d sell_in=5 -d quality=8
-        // http://192.168.0.19:5000/items
+// DELETE
+// curl -d name="Conjured Mana Cake" -d sell_in=5 -d quality=8
+// http://127.0.0.1/items -X DELETE
 
-        //let data = {name: "Conjured Mana Cake",
-          //          sell_in: 5,
-          //          quality: 8};
+formulario.delete.addEventListener('click', deleteItemPromise);
 
-        let formulario = document.querySelector('.add-item');
-        formulario.addEventListener('submit', addItem);
+function deleteItem() {
+    // elementos del formulario en un array-like object:
+    // form.elements 
+    // pag 396 libro rhino
+    logForm();
 
-        function addItem(e) {
-            e.preventDefault();
-            // elementos del formulario en un array-like object:
-            // form.elements 
-            // pag 396 libro rhino
-            logForm();
+    let data = {
+        name: formulario.elements.name.value,
+        // sellin => bad request, status 400
+        // sin valor => mensaje del servidor en consola
+        // en XHR and fetch: message: {sell_in: "sellIn required"}
+        sell_in: formulario.elements.sell_in.value,
+        quality: formulario.elements.quality.value
+    };
 
-            let data = { name: this.elements.name.value,
-                         // sellin => bad request, status 400
-                         // sin valor => mensaje del servidor en consola
-                         // en XHR and fetch: message: {sell_in: "sellIn required"}
-                         sell_in: this.elements.sell_in.value,
-                         quality: this.elements.quality.value};
+    fetch('http://127.0.0.1:5000/items', {
+        method: 'DELETE',
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((response) => {
+            if (response.ok) {
+                console.log("Response OK Status:", response.status);
+                console.log("Reponse OK status text:", response.statusText);
+            } else {
+                console.log("Response Status:", response.status);
+                console.log("Reponse statuts text:", response.statusText);
+            }
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+}
 
-            fetch('http://127.0.0.1:5000/items', {
-                method: 'POST', // or 'PUT'
-                body: JSON.stringify(data), // data can be `string` or {object}!
-                headers: {
-                    'Content-Type': 'application/json'
+// delete Item con promesas
+
+function deleteItemPromise() {
+    // elementos del formulario en un array-like object:
+    // form.elements 
+    // pag 396 libro rhino
+    logForm();
+
+    let data = {
+        name: formulario.elements.name.value,
+        // sellin => bad request, status 400
+        // sin valor => mensaje del servidor en consola
+        // en XHR and fetch: message: {sell_in: "sellIn required"}
+        sell_in: formulario.elements.sell_in.value,
+        quality: formulario.elements.quality.value
+    };
+
+    const promesa = new Promise((resolve, reject) => {
+
+        fetch('http://127.0.0.1:5000/items', {
+            method: 'DELETE',
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    setTimeout(() => { resolve(response.statusText) }, 5000);
+                } else {
+                    reject(response.statusText);
                 }
             })
-                .then((response) => {
-                    if (response.ok) {
-                        console.log("Response OK Status:", response.status);
-                        console.log("Reponse OK status text:", response.statusText);
-                    } else {
-                        console.log("Response Status:", response.status);
-                        console.log("Reponse statuts text:", response.statusText);
-                    }
-                })
-                .catch((error) => {
-                    console.log(error.message);
-                });
-        }
-      
-        // DELETE
-        // curl -d name="Conjured Mana Cake" -d sell_in=5 -d quality=8
-        // http://127.0.0.1/items -X DELETE
+    });
 
-        formulario.delete.addEventListener('click', deleteItemPromise);
+    promesa.then(statusText => console.log(`"Item eliminado: ${statusText}`))
+        .then(() => inventario())
+        .catch((statusText) => {
+            console.log(`Item NO borrado: ${statusText}`);
+        });
 
-        function deleteItem() {
-            // elementos del formulario en un array-like object:
-            // form.elements 
-            // pag 396 libro rhino
-            logForm();
-
-            let data = { name: formulario.elements.name.value,
-                         // sellin => bad request, status 400
-                         // sin valor => mensaje del servidor en consola
-                         // en XHR and fetch: message: {sell_in: "sellIn required"}
-                         sell_in: formulario.elements.sell_in.value,
-                         quality: formulario.elements.quality.value };
-
-            fetch('http://127.0.0.1:5000/items', {
-                method: 'DELETE',
-                body: JSON.stringify(data), // data can be `string` or {object}!
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        console.log("Response OK Status:", response.status);
-                        console.log("Reponse OK status text:", response.statusText);
-                    } else {
-                        console.log("Response Status:", response.status);
-                        console.log("Reponse statuts text:", response.statusText);
-                    }
-                })
-                .catch((error) => {
-                    console.log(error.message);
-                });
-        }
-
-        // delete Item con promesas
-        
-        function deleteItemPromise() {
-            // elementos del formulario en un array-like object:
-            // form.elements 
-            // pag 396 libro rhino
-            logForm();
-
-            let data = { name: formulario.elements.name.value,
-                         // sellin => bad request, status 400
-                         // sin valor => mensaje del servidor en consola
-                         // en XHR and fetch: message: {sell_in: "sellIn required"}
-                         sell_in: formulario.elements.sell_in.value,
-                         quality: formulario.elements.quality.value };
-            
-            const promesa = new Promise((resolve, reject) => {
-                    
-                    fetch('http://127.0.0.1:5000/items', {
-                        method: 'DELETE',
-                        body: JSON.stringify(data), // data can be `string` or {object}!
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    .then((response) => {
-                        if (response.ok) {
-                            setTimeout(() => { resolve(response.statusText)}, 5000);
-                        } else {
-                            reject(response.statusText);
-                        }
-                    })
-                });
-            
-            promesa.then(statusText => console.log(`"Item eliminado: ${statusText}`))
-                   .then(() => inventario())
-                   .catch((statusText) => {
-                        console.log(`Item NO borrado: ${statusText}`);
-                    });
-
-            console.log("El código sigue sin esperar al setTimeout");              
-        }
+    console.log("El código sigue sin esperar al setTimeout");
+}
 
 
-        function logForm() {
-            // for sobre form.elements no sirve
-            // porque muestra buttons tb
-            let formulario = document.querySelector('.add-item');
-            console.log( formulario.elements.name.value,
-                         formulario.elements.sell_in.value,
-                         formulario.elements.quality.value);
-        }
-
-    </script>
+function logForm() {
+    // for sobre form.elements no sirve
+    // porque muestra buttons tb
+    let formulario = document.querySelector('.add-item');
+    console.log(formulario.elements.name.value,
+        formulario.elements.sell_in.value,
+        formulario.elements.quality.value);
+}
