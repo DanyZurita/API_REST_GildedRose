eacf9d94998c4bae35539246cfeb5d752bd58317
DanyZurita <dzurita@cifpfbmoll.eu>
2020-03-22 20:17:17 +0100

    feat(db): Añadir la url de mongo atlas

--- repository/db_atlas.py
+++ repository/db_atlas.py
@@ -21,7 +21,7 @@ def get_db():
         # Conectarse a Mongo Atlas con mongoengine
         # En la URI, /test? es la bbdd
         g.db = connect(
-                    host='mongodb+srv://<usuario>:<password>@api-rest-wlrfx.mongodb.net/test?retryWrites=true&w=majority'
+                    host='mongodb+srv://ollivander:gildedrose@pruebas-icv6e.mongodb.net/Ollivander?retryWrites=true&w=majority'
                 )
         # Item necesita encontrar la referencia la conexión
         # devuelta por connect()
